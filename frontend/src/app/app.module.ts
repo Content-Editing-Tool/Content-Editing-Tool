ed26434ae60a28f237fc89e7e6213fbea98698e5
Ansar <azhdarov.ansar@gmail.com>
2023-11-20 13:13:10 +0100

    new backend added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ModificationtoolComponent} from "./modificationtool/modificationtool.component";
@@ -12,7 +12,8 @@ import {ModificationtoolComponent} from "./modificationtool/modificationtool.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [ModificationtoolComponent]

a0d700ff7cb9440088ca1a43775c703ca91e84e3
Ștefan Popescu <stefan.popescu@student.fontys.nl>
2023-11-22 11:21:25 +0100

    Solve merge issue

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
+
 import { HttpClientModule } from  '@angular/common/http';
-=======
-import {HttpClientModule} from '@angular/common/http';
->>>>>>> 92653d593bfd813afe455a814cc388d28441dd1e
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ModificationtoolComponent} from "./modificationtool/modificationtool.component";
